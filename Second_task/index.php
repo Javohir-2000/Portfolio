a8b146504baad474a0f444711cd9fd0543489148
Javohir <cybermanrajapboyev@gmail.com>
2021-04-28 11:01:22 +0300

    Konets

---
+++ Second_task/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <style type="text/css">
+    .banner
+    {
+    display: block;
+    max-width:200px;
+    max-height:100px;
+    width: auto;
+    height: auto;
+    }
+    </style>
+    </head>
+    <body>
+        <img class="banner" src="tree.jpg">
+    </body>
+</html>
